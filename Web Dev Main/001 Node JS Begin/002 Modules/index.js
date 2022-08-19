b463e2532d62ff25517453ffd08a8c59babd6d29
maityamit <maityamit308@gmail.com>
2022-08-20 04:47:35 +0530

    node js learn

---
+++ Web Dev Main/001 Node JS Begin/002 Modules/index.js
@@ -0,0 +1,5 @@
+const operations = require('./operation')
+
+console.log(operations.add(5,8))
+console.log(operations.multiply(2,3))
+
---
+++ Web Dev Main/001 Node JS Begin/index.js
@@ -0,0 +1,9 @@
+console.log("Hello Node JS")
+
+function add(a,b){
+    return a+b
+}
+
+console.log(add(5,3))
+
+console.log(process.argv)
